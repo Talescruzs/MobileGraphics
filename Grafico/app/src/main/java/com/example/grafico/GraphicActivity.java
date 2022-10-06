051a76cfbf8cf93117d19447a8936c9a682235e4
guikage <guieinloft@protonmail.com>
2022-10-06 20:20:27 -0300

    abandonando o androidplot

--- Grafico/app/src/main/java/com/example/grafico/GraphicActivity.java
+++ Grafico/app/src/main/java/com/example/grafico/GraphicActivity.java
@@ -3,22 +3,11 @@ package com.example.grafico;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
+import java.lang.Math;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.androidplot.xy.BoundaryMode;
-import com.androidplot.xy.CatmullRomInterpolator;
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYGraphWidget;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
-
-import java.text.FieldPosition;
-import java.text.Format;
-import java.text.ParsePosition;
-import java.util.Arrays;
 
 
 public class GraphicActivity extends AppCompatActivity {
@@ -31,18 +20,19 @@ public class GraphicActivity extends AppCompatActivity {
     float m;
     float n;
 
+    float angulo;
+
     float interseptY;
     float interseptX;
 
-    private XYPlot plot;
+    ImageView reta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graphic);
 
-        plot = (XYPlot) findViewById(R.id.plot);
-        plot.setDomainBoundaries(null, null, BoundaryMode.AUTO);
+        reta = findViewById(R.id.reta);
 
         Intent getData = getIntent();
         isGeral = getData.getBooleanExtra(MainActivity.TYPE, true);
@@ -59,37 +49,11 @@ public class GraphicActivity extends AppCompatActivity {
         interseptX = (n*-1)/m;
         interseptY = n;
 
-        Number[] domainLabels = {interseptX*2, interseptX, 0, interseptX*-1};
-        Number[] series1Numbers = {interseptY*-1, 0, interseptY, interseptY*2};
+        angulo = (float) Math.atan(m);
+        angulo = (float) Math.toDegrees(angulo);
 
-        XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Reta inserida");
+        reta.setRotation(-angulo);
 
-        // create formatters to use for drawing a series using LineAndPointRenderer
-        // and configure them from xml:
-        LineAndPointFormatter series1Format =
-                new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels);
-
-
-        // just for fun, add some smoothing to the lines:
-        // see: http://androidplot.com/smooth-curves-and-androidplot/
-        series1Format.setInterpolationParams(
-                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
-
-        // add a new series' to the xyplot:
-        plot.addSeries(series1, series1Format);
-
-        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
-            @Override
-            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
-                int i = Math.round(((Number) obj).floatValue());
-                return toAppendTo.append(domainLabels[i]);
-            }
-            @Override
-            public Object parseObject(String source, ParsePosition pos) {
-                return null;
-            }
-        });
 
     }
     public void teste(View v) {
